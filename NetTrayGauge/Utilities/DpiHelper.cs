0185f2db53d1d73e20e7cbe7678d9510254b8a8f
Tobias <moosfett86@gmail.com>
2025-12-02 21:49:48 +0100

    Improve DPI scale detection

--- NetTrayGauge/Utilities/DpiHelper.cs
+++ NetTrayGauge/Utilities/DpiHelper.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media;
 
 namespace NetTrayGauge.Utilities;
@@ -10,12 +12,68 @@ public static class DpiHelper
 {
     public static double GetScale()
     {
-        var source = PresentationSource.FromVisual(Application.Current.MainWindow ?? (System.Windows.Media.Visual?)null);
+        var visual = GetAvailableVisual();
+        if (visual != null)
+        {
+            return GetScaleFromVisual(visual);
+        }
+
+        using var temporarySource = CreateHiddenSource();
+        if (temporarySource?.RootVisual is Visual temporaryRoot)
+        {
+            return GetScaleFromVisual(temporaryRoot);
+        }
+
+        return VisualTreeHelper.GetDpi(new DrawingVisual()).DpiScaleX;
+    }
+
+    private static Visual? GetAvailableVisual()
+    {
+        var application = Application.Current;
+        if (application == null)
+        {
+            return null;
+        }
+
+        if (IsUsableVisual(application.MainWindow))
+        {
+            return application.MainWindow!;
+        }
+
+        return application.Windows
+            .OfType<Window>()
+            .FirstOrDefault(IsUsableVisual);
+    }
+
+    private static bool IsUsableVisual(Window? window) => window is { IsLoaded: true };
+
+    private static double GetScaleFromVisual(Visual visual)
+    {
+        var source = PresentationSource.FromVisual(visual);
         if (source?.CompositionTarget != null)
         {
             return source.CompositionTarget.TransformToDevice.M11;
         }
 
-        return 1.0;
+        return VisualTreeHelper.GetDpi(visual).DpiScaleX;
+    }
+
+    private static HwndSource? CreateHiddenSource()
+    {
+        var parameters = new HwndSourceParameters("DpiHelperHiddenWindow")
+        {
+            Width = 0,
+            Height = 0,
+            PositionX = 0,
+            PositionY = 0,
+            WindowStyle = 0
+        };
+
+        var source = new HwndSource(parameters)
+        {
+            RootVisual = new DrawingVisual()
+        };
+
+        return source;
     }
 }
